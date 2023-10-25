d06c84a4e9e551e7bcf842d84c0d07355ff160ab
EdwardDifert <130516207+EdwardDifert@users.noreply.github.com>
2023-10-25 09:14:42 -0300

    Add files via upload

---
+++ Curso JS DOM/js/main.js
@@ -0,0 +1,34 @@
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    
+    elemento.addEventListener("click", () => {
+      manipulaDados(evento.target.textContent)
+    })
+    
+})
+
+
+    
+
+
+function manipulaDados(operacao) {
+    if (operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+
+    }
+}
+
+    
+
+
+
+
+
+
